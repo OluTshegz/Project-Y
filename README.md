64ec50f94de66271e60b48d975d3547b59666f8c
Olusegun <ayosegun2001@gmail.com>
2024-10-07 15:54:34 +0100

    First commit: MongoDB connected successfully with some dummy routes and data AND frontend folder structure with REACT

---
+++ README.md
@@ -0,0 +1,3 @@
+<!-- Project `Y` README.md file -->
+# Y --- The ex (X) of Twitter, X-Twitter OR ex-Twitter
+## A clone of X: X-clone
---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { signup, login, logout } from "../controllers/auth.controllers.js";
+
+const authRoutes = express.Router();
+
+authRoutes.get("/signup", signup);
+
+authRoutes.get("/login", login);
+
+authRoutes.get("/logout", logout);
+
+export default authRoutes;
